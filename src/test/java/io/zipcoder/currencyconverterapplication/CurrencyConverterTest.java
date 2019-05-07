e9887728419f9cd82f7645b5796fd9b6fbe00452
Leon Hunter <leonhunter@Leons-MacBook-Pro.local>
2019-05-07 14:17:17 -0400

    updated tests

--- src/test/java/io/zipcoder/currencyconverterapplication/CurrencyConverterTest.java
+++
@@ -1,8 +0,0 @@
-package io.zipcoder.currencyconverterapplication;
-
-
-public class CurrencyConverterTest {
-    public Double convert(Double amountOfBaseCurrency, ConvertableCurrency currency, CurrencyType currencyTypeToConvertTo) {
-        return currency.convert(currencyTypeToConvertTo) * amountOfBaseCurrency;
-    }
-}
--- src/test/java/io/zipcoder/currencyconverterapplication/currencyconverter/YenCurrencyConverterTest.java
+++ src/test/java/io/zipcoder/currencyconverterapplication/currencyconverter/YenCurrencyConverterTest.java
@@ -1,7 +1,100 @@
 package io.zipcoder.currencyconverterapplication.currencyconverter;
 
+import io.zipcoder.currencyconverterapplication.ConvertableCurrency;
+import io.zipcoder.currencyconverterapplication.CurrencyConverter;
+import io.zipcoder.currencyconverterapplication.CurrencyType;
+import io.zipcoder.currencyconverterapplication.currencies.USDollar;
+import io.zipcoder.currencyconverterapplication.currencies.Yen;
+import org.junit.Assert;
+import org.junit.Test;
+
 /**
  * @author leonhunter
  */
 public class YenCurrencyConverterTest {
+    /**
+     * template method to be called by other test methods
+     * @param amountOfBaseCurrency - amount of the base currency to be converted
+     * @param targetCurrency - enumeration of the target currency type
+     * @param expected - expected amount in target current
+     */
+    private void testConversion(Double amountOfBaseCurrency, CurrencyType targetCurrency, Double expected) {
+        // given
+        ConvertableCurrency sourceCurrency = new Yen();
+
+        // when
+        Double actual = CurrencyConverter.convert(amountOfBaseCurrency, sourceCurrency, targetCurrency);
+
+        // then
+        Assert.assertEquals(expected, actual, .0001);
+    }
+
+    /**
+     * template method to be called by other test methods
+     * @param targetCurrency type of currency to convert to
+     * @param expected rate to expect
+     */
+    private void testConversion(CurrencyType targetCurrency, Double expected) {
+        testConversion(10.0, targetCurrency, expected);
+    }
+
+    @Test
+    public void testYenConversion() {
+        testConversion(CurrencyType.YEN, 10.0);
+    }
+
+    @Test
+    public void testAustralianConversion() {
+        testConversion(CurrencyType.AUSTRALIAN_DOLLAR, .1165400552486188);
+    }
+
+    @Test
+    public void testCanadianDollarConversion() {
+        testConversion(CurrencyType.CANADIAN_DOLLAR, .11395027624309393);
+    }
+
+    @Test
+    public void testChineseYRConversion() {
+        testConversion(CurrencyType.CHINESE_YR, .5973756906077348);
+    }
+
+    @Test
+    public void testEuroConversion() {
+        testConversion(CurrencyType.EURO, .081146408839779);
+    }
+
+    @Test
+    public void testFrancConversion() {
+        testConversion(CurrencyType.FRANC, .08718922651933702);
+    }
+
+    @Test
+    public void testPoundConversion() {
+        testConversion(CurrencyType.POUND, .07078729281767956);
+    }
+
+    @Test
+    public void testRinggitConversion() {
+        testConversion(CurrencyType.RINGGIT, .3858770718232044);
+    }
+
+    @Test
+    public void testRupeeConversion() {
+        testConversion(CurrencyType.RUPEE, 5.897790055248618);
+    }
+
+    @Test
+    public void testSingaporeConversion() {
+        testConversion(CurrencyType.SINGAPORE_DOLLAR, .08028314917127072);
+    }
+
+    @Test
+    public void testUniversalCurrencyConversion() {
+        testConversion(CurrencyType.UNIVERSAL_CURRENCY, .04316298342541437);
+    }
+
+    @Test
+    public void testUSDollarConversion() {
+        testConversion(CurrencyType.US_DOLLAR, .08632596685082873);
+    }
 }
